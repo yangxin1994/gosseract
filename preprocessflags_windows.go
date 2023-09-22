d67e903b6b86bab35d3d284f5f19f6ba2af3aa17
Hiromu OCHIAI <otiai10@gmail.com>
2023-09-23 00:51:57 +0900

    Fix

--- preprocessflags_windows.go
+++ preprocessflags_windows.go
@@ -2,5 +2,5 @@ package gosseract
 
 // #cgo CXXFLAGS: -std=c++0x
 // #cgo CPPFLAGS: -Wno-unused-result -IC:/vcpkg/installed/x64-windows/include
-// #cgo LDFLAGS: -L:C:/vcpkg/installed/x64-windows/lib -l:leptonica-1.84.0.lib -l:tesseract53.lib
+// #cgo LDFLAGS: -LC:/vcpkg/installed/x64-windows/lib -l:leptonica-1.84.0.lib -l:tesseract53.lib
 import "C"
